72636c720aab7ec6cb090852d2863611d90a2b43
Roman Heinrich <roman.heinrich@gmail.com>
2013-02-20 16:04:22 +0200

    openresty with compiling works

--- puppet/modules/openresty/manifests/init.pp
+++ puppet/modules/openresty/manifests/init.pp
@@ -16,10 +16,23 @@ define openresty::package{
 
 
 class openresty::download{
+  exec{"openresty::download":
+    command => "wget $openresty::params::url",
+    cwd     => "/tmp",
+    unless  => "test -e /tmp/$openresty::params::filename"
+  }
+
+  -> exec{"openresty::untar":
+    command => "tar xvfz /tmp/$openresty::params::filename",
+    unless  => "test -e /tmp/$openresty::params::folder"
+  }
 
 }
 
-
 class openresty::install{
-
+  exec{"openresty::install":
+    command => "echo 1 && ./configure && make && make install",
+    cwd     => "/tmp/$openresty::params::folder",
+    unless  => "test -e /usr/local/openresty"
+  }
 }
--- puppet/modules/openresty/manifests/params.pp
+++ puppet/modules/openresty/manifests/params.pp
@@ -1,10 +1,16 @@
 class openresty::params{
   # http://openresty.org/download/ngx_openresty-1.2.6.6.tar.gz
+  $version  = "1.2.6.6"
+  $folder   = "ngx_openresty-$version"
+  $filename = "$folder.tar.gz"
+  $url      = "http://openresty.org/download/$filename"
+
   $packages = [
+    'wget',
     'build-essential',
     'libncurses5',
     'libncurses5-dev',
     'libpcre3',
-    'libpcre3-dev'
+    'libpcre3-dev',
   ]
 }
