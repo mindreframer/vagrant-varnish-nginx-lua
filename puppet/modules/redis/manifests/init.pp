793dc0a36f5e8282aa6e495a7f9b32c743010460
Roman Heinrich <roman.heinrich@gmail.com>
2013-02-21 21:43:50 +0200

    testing runit for redis

--- puppet/modules/redis/manifests/init.pp
+++ puppet/modules/redis/manifests/init.pp
@@ -18,26 +18,45 @@ define redis::package{
 class redis::download{
   exec{"redis::download":
     command => "wget $redis::params::url",
-    cwd     => "/tmp",
-    unless  => "test -e /tmp/$redis::params::filename"
+    cwd     => "/var/tmp",
+    unless  => "test -e /var/tmp/$redis::params::filename"
   }
 
   -> exec{"redis::untar":
-    command => "tar xvfz /tmp/$redis::params::filename",
-    unless  => "test -e /tmp/$redis::params::folder"
+    command => "tar xvfz /var/tmp/$redis::params::filename",
+    cwd     => "/var/tmp",
+    unless  => "test -e /var/tmp/$redis::params::folder"
   }
 }
 
 class redis::install{
   exec{"redis::install":
     command => "echo 1 && make && make install",
-    cwd     => "/tmp/$redis::params::folder",
+    cwd     => "/var/tmp/$redis::params::folder",
     unless  => "test -e /usr/local/bin/redis-server"
   }
 }
 
 class redis::configs{
-
+  file{
+    ["/etc/sv/redis-server",
+    "/etc/sv/redis-server/log"]:
+    ensure => directory
+  }
+  -> file{"/etc/sv/redis-server/run":
+    content => "#!/bin/bash \
+exec 2>&1 \
+exec redis-server",
+    mode    => 0755
+  }
+  -> file{"/etc/sv/redis-server/log/run":
+    content => "#!/bin/bash \
+\
+set -e \
+exec 2>&1 \
+exec svlogd -tt -b 10000 .",
+    mode => 0755
+  }
 }
 
 class redis::service{
