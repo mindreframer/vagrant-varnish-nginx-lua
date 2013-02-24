9276eaa6cbe2f43cfa4771b97d31b46d10afae97
Roman Heinrich <roman.heinrich@gmail.com>
2013-02-24 19:43:14 +0200

    use esi

--- your_project/app/controllers/home.rb
+++ your_project/app/controllers/home.rb
@@ -4,24 +4,7 @@ YourProject::App.controllers :home do
     render 'home/index'
   end
 
-  # get :index, :map => '/foo/bar' do
-  #   session[:foo] = 'bar'
-  #   render 'index'
-  # end
-
-  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
-
-  # get :foo, :with => :id do
-  #   'Maps to url '/foo/#{params[:id]}''
-  # end
-
-  # get '/example' do
-  #   'Hello world!'
-  # end
-
-
+  get :esi do
+    "esi content, wowowo"
+  end
 end
