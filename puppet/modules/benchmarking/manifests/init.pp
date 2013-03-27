0441016cba6176b67e433108b013032ba7a8956e
Roman Heinrich <roman.heinrich@gmail.com>
2013-03-27 22:26:12 +0200

    more inspection tools

--- puppet/modules/benchmarking/manifests/init.pp
+++ puppet/modules/benchmarking/manifests/init.pp
@@ -2,4 +2,7 @@ class benchmarking{
   package { "apache2-utils":
     ensure => installed,
   }
+  package{"gdb": ensure => installed}
+  package{"telnet": ensure => installed}
+  package{"lsof": ensure => installed}
 }
