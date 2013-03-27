ca95439c07ee58d84abe3c965acf7bf2c6d7f702
Roman Heinrich <roman.heinrich@gmail.com>
2013-03-27 22:26:00 +0200

    upgrade redis

--- puppet/modules/redis/manifests/params.pp
+++ puppet/modules/redis/manifests/params.pp
@@ -1,6 +1,6 @@
 class redis::params{
   # http://redis.googlecode.com/files/redis-2.6.10.tar.gz
-  $version  = "2.6.10"
+  $version  = "2.6.11"
   $folder   = "redis-$version"
   $filename = "$folder.tar.gz"
   $url      = "http://redis.googlecode.com/files/$filename"
