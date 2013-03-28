2964daa0654aef4dc616169711a93556aa4456be
Roman Heinrich <roman.heinrich@gmail.com>
2013-03-28 03:17:49 +0200

    the latest version for openresty

--- puppet/modules/openresty/manifests/params.pp
+++ puppet/modules/openresty/manifests/params.pp
@@ -1,7 +1,7 @@
 class openresty::params{
   # http://openresty.org/download/ngx_openresty-1.2.6.6.tar.gz
-  $version  = "1.2.6.6"
-  #$version  = "1.2.7.3"
+  # $version  = "1.2.6.6"
+  $version  = "1.2.7.3"
   $folder   = "ngx_openresty-$version"
   $filename = "$folder.tar.gz"
   $url      = "http://openresty.org/download/$filename"
